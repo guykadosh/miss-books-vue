f7d8f56f5b5146492f9cda8b5a4561cb7c9bba3a
Guy Kadosh <guyk50@gmail.com>
2022-06-27 22:14:32 +0300

    added home-page

--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -1,7 +1,12 @@
 export default {
   template: `
-  <section class="home-page app-main">
-    <h3>Home sweet home</h3>
+  <section class="home-page">
+    <div class="headings">
+      <h1>Welcome to MissBooks Backoffice</h1>
+      <h2>Manage your book store daily and easily!</h2>
+
+    </div>
+    <img src="../img/home-page.svg" alt="">
  </section>
 `,
   data() {
